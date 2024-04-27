0096e1c636598246683ca7d599e6538d121e466a
marrieh_PLPAcademy <maryw0990@gmail.com>
2024-04-27 23:02:31 +0300

    added a paragraph on my index.html file

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,9 +25,11 @@ SECRET_KEY = 'django-insecure-4ju2n@$f9d0c=h)_g0lbb%k9&@rf(xa$d$g$&5ri$uf)*gev^4
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '127.0.0.1:8000']
 
-ALLOWED_HOSTS = [".replit.dev", ".replit.app"]
-CSRF_TRUSTED_ORIGINS = ["https://*.replit.dev", "https://*.replit.app"]
+#ALLOWED_HOSTS = [".replit.dev", ".replit.app"]
+#CSRF_TRUSTED_ORIGINS = ["https://.replit.dev", "https://.replit.app"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
